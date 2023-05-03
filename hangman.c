8db8a2ad86d63741ae5acd1f9deb32a7ed36b7bd
kamiljochym <kamiljochym@gmail.com>
2023-05-03 19:34:04 +0200

    added hangman drawing ascii art

--- hangman.c
+++ hangman.c
@@ -13,15 +13,20 @@
 #define DEAD 0
 
 int get_char_in_word(int c, int length, char word[], char *revealed_word,
-                     int *correct_letters) {
+                     int *correct_letters, int *lives) {
+  int wrong_letter = 0;
   for (int i = 0; i < length; ++i) {
     if (c == word[i]) {
       /* printf("Letter is in word"); */
       revealed_word[i] = c;
       *correct_letters += 1;
+      wrong_letter += 1;
     } else {
       /* printf("letter not in word"); */
     }
+  }
+  if (wrong_letter == 0) {
+    *lives -= 1;
   }
   return 0;
 }
@@ -44,6 +49,19 @@ int fill_array(char *revealed_word, int length) {
 }
 
 int main(void) {
+
+  char hangman_states[][100] = {
+      "   _____  \n   |   |  \n   o   |  \n  /|\\  |  \n  / \\  |  \n",
+      "   _____  \n       |  \n   o   |  \n  /|\\  |  \n  / \\  |  \n",
+      "   _____  \n       |  \n   o   |  \n  /|\\  |  \n  /    |  \n",
+      "   _____  \n       |  \n   o   |  \n  /|\\  |  \n       |  \n",
+      "   _____  \n       |  \n   o   |  \n  /|   |  \n       |  \n",
+      "   _____  \n       |  \n   o   |  \n   |   |  \n       |  \n",
+      "   _____  \n       |  \n   o   |  \n       |  \n       |  \n",
+      "   _____  \n       |  \n       |  \n       |  \n       |  \n",
+
+  };
+
   int c;
   int game_state;
   game_state = ALIVE;
@@ -52,6 +70,7 @@ int main(void) {
   int correct_letters = 0;
   char word[30];
   char revealed_word[30];
+  int lives = 8;
 
   // user inputs word to be guessed
   printf("Enter word to be guessed: \n");
@@ -71,7 +90,8 @@ int main(void) {
       continue;
     }
 
-    get_char_in_word(c, length_of_word, word, revealed_word, &correct_letters);
+    get_char_in_word(c, length_of_word, word, revealed_word, &correct_letters,
+                     &lives);
 
     game_state = check_win(word, revealed_word, length_of_word);
 
@@ -80,7 +100,56 @@ int main(void) {
       game_state = DEAD;
       break;
     }
-    printf("%s\n", revealed_word);
+
+    if (lives < 8) {
+      printf("%s", hangman_states[lives]);
+    }
+    if (lives == 0) {
+      game_state = DEAD;
+      printf("YOU LOSE!");
+      break;
+    }
+    printf("%s, %d \n", revealed_word, lives);
     printf("Guess letter: ");
   }
 }
+
+/*                   "   _____  \n", */
+/*                   "       |  \n", */
+/*                   "   o   |  \n", */
+/*                   "  /|\  |  \n", */
+/*                   "  / \  |  \n", */
+/*                   "   _____  \n", */
+/*                   "       |  \n", */
+/*                   "   o   |  \n", */
+/*                   "  /|\  |  \n", */
+/*                   "  /    |  \n", */
+
+/*                   "   _____  \n", */
+/*                   "       |  \n", */
+/*                   "   o   |  \n", */
+/*                   "  /|\  |  \n", */
+/*                   "       |  \n", */
+
+/*                   "   _____  \n", */
+/*                   "       |  \n", */
+/*                   "   o   |  \n", */
+/*                   "  /|   |  \n", */
+/*                   "       |  \n", */
+
+/*                   "   _____  \n", */
+/*                   "       |  \n", */
+/*                   "   o   |  \n", */
+/*                   "   |   |  \n", */
+/*                   "       |  \n", */
+/*                   "   _____  \n", */
+/*                   "       |  \n", */
+/*                   "   o   |  \n", */
+/*                   "       |  \n", */
+/*                   "       |  \n", */
+
+/*                   "   _____  \n", */
+/*                   "       |  \n", */
+/*                   "       |  \n", */
+/*                   "       |  \n", */
+/*                   "       |  \n", */
